b2d916917b1ae9862b0c1586e294b0b205c97c55
Matviy <school@A42PC5>
2025-04-08 14:27:43 +0300

    vcbcvbcvb

--- Coffee-machine/Program.cs
+++ Coffee-machine/Program.cs
@@ -18,57 +18,83 @@ Menu menu = new();
 
 CoffeeMachine1 coffee_Machine = new();
 
-while (true)
+menu.ShowLoginmenu();
+int roleChoice = menu.AskUserInput();
+
+if(roleChoice == 1)
 {
-    Console.Clear();
+    Console.Clear(); 
+    Console.WriteLine("------------- Welcome to Supermarket for User! -------------");
 
-    menu.ShowMenu();
+    menu.ShowUsermenu();
     int choice = menu.AskUserInput();
-
 
     switch (choice)
     {
-        case 0:
-            Console.WriteLine("PAKA!");
-            return;
-            
-
+        case 0: return 0;
         case 1:
-            coffee_Machine.AddNewDrinkWeight();
-            break;
-
-       
-        case 2:
-            coffee_Machine.Drinkname();
-            break;
-
-        case 3:
-            coffee_Machine.AddDrink();
-            break;
-
-        case 4:
-            coffee_Machine.Deletedrink();
-            break;
-
-        case 5:
-            coffee_Machine.SaveAll();
-            break;
-            
-        case 6:
-            coffee_Machine.ShowDrink();
-            break;
-        case 7:
-            coffee_Machine.ShowComponens();
-            break;
-
-        case 9:
-            coffee_Machine.FindDrink();
-            break;
-        case 10:
-            coffee_Machine.LoadDrinks();
+            Console.WriteLine("You can buy something!");
             break;
     }
-    Console.WriteLine("Натистінь щось для продовження...");
-    Console.ReadKey();
 }
 
+if (roleChoice == 2)
+{
+
+    while (true)
+    {
+        Console.Clear();
+
+        menu.Showmenu();
+        int choice = menu.AskUserInput();
+
+
+        switch (choice)
+        {
+            case 0:
+                Console.WriteLine("PAKA!");
+                break;
+
+
+            case 1:
+                coffee_Machine.AddNewDrinkWeight();
+                break;
+
+
+            case 2:
+                coffee_Machine.Drinkname();
+                break;
+
+            case 3:
+                coffee_Machine.AddDrink();
+                break;
+
+            case 4:
+                coffee_Machine.Deletedrink();
+                break;
+
+            case 5:
+                coffee_Machine.SaveAll();
+                break;
+
+            case 6:
+                coffee_Machine.ShowDrink();
+                break;
+            case 7:
+                coffee_Machine.ShowComponens();
+                break;
+
+            case 9:
+                coffee_Machine.FindDrink();
+                break;
+            case 10:
+                coffee_Machine.LoadDrinks();
+                break;
+        }
+        Console.WriteLine("Натистінь щось для продовження...");
+        Console.ReadKey();
+    }
+
+}
+Console.WriteLine("Гарного дня!");
+return 0;
--- Coffee-machine/menu.cs
+++ Coffee-machine/menu.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Coffee_machine
 {
-    internal class menu
+    internal class Menu
     {
+        public void Showmenu()
+        {
+            Console.WriteLine("--------------------------Hot drinks machine--------------------------");
+            Console.WriteLine("[[[[[ ADMIN MENU ]]]]]\r\n" +
+                 "1.Loading the machine with water, coffee, tea, sugar\r\n" +
+                 "2.Output of statistics on the availability of ingredients for making drinks\r\n" +
+                 "3.Change in prices for drinks\r\n" +
+                 "4.Removing the cache\r\n" +
+                 "5.Save Settings\r\n" +
+                 "6.Show All\r\n" +
+                 "4.Add new drink\r\n" +
+                 "5.Removing the cache\r\n" +
+                 "6.Save Settings\r\n" +
+                 "7.Show Drink\r\n" +
+                 "8.Show Componens\r\n" +
+                 "9.Find Drink\r\n" +
+                "----------------------------------------------------------------------");
+        }
+        public void ShowLoginmenu()
+        {
+            Console.WriteLine("--------------------------Welcome--------------------------");
+            Console.WriteLine("[[[[[ WHAT YOU ARE ]]]]]\r\n" +
+                 "0.Exit\r\n" +
+                 "1.User\r\n" +
+                 "2.Admin\r\n" +
+                 
+                "----------------------------------------------------------------------");
+        }
+
+        public void ShowUsermenu()
+        {
+            Console.WriteLine("--------------------------USER MENU--------------------------");
+            Console.WriteLine("\tUSER MENU\n" +
+                          "0. Exit\n" +
+                          "1. Buy Drink");
+            Console.WriteLine("______________________________");
+        }
+        public int AskUserInput()
+        {
+            Console.Write("Your choice: ");
+            return int.Parse(Console.ReadLine());
+        }
     }
 }
